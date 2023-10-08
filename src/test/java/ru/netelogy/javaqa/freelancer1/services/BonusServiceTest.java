7e613e6efb30e6fe1940639f47dc5a3976593398
leily <leily@example.com>
2023-10-08 21:52:49 +0300

    Parameterized Test

--- src/test/java/ru/netelogy/javaqa/freelancer1/services/BonusServiceTest.java
+++ src/test/java/ru/netelogy/javaqa/freelancer1/services/BonusServiceTest.java
@@ -1,29 +1,31 @@
 package ru.netelogy.javaqa.freelancer1.services;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-//import ru.netelogy.javaqa.freelancer1.services.BonusService;
+//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+//import org.junit.jupiter.params.provider.CsvSource;
+
 
 public class BonusServiceTest {
 
-    @Test
-    void shouldCalculateForOneExample() {
-        BonusService service = new BonusService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-        int actual = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/bonus.csv")
 
-    }
-    @Test
-    void shouldCalculateForTwoExample() {
+    public void shouldCalculateForExample(int expected, int income, int expenses, int threshold) {
         BonusService service = new BonusService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
+
+
+//@Test
+//void shouldCalculateForTwoExample() {
+//BonusService service = new BonusService();
+//int income = 10_000;
+//int expenses = 3_000;
+//int threshold = 20_000;
+//int expected = 3;
+//int actual = service.calculate(income, expenses, threshold);
+//Assertions.assertEquals(expected, actual);
 }
