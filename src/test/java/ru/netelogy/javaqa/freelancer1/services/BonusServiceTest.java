fddbe426e7ef3eb6154c83d702ec7fa5fc3af3e6
leily <leily@example.com>
2023-10-10 10:45:45 +0300

    Remove the underlining

--- src/test/java/ru/netelogy/javaqa/freelancer1/services/BonusServiceTest.java
+++ src/test/java/ru/netelogy/javaqa/freelancer1/services/BonusServiceTest.java
@@ -1,10 +1,8 @@
 package ru.netelogy.javaqa.freelancer1.services;
 
 import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-//import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class BonusServiceTest {
@@ -12,7 +10,7 @@ public class BonusServiceTest {
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/bonus.csv")
 
-    public void shouldCalculateForExample(int expected, int income, int expenses, int threshold) {
+    public void shouldCalculate(int expected, int income, int expenses, int threshold) {
         BonusService service = new BonusService();
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
